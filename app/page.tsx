af9f5d715bc55d1bfadb6716790fe911ff414c2c
taufikemal <artcrev7@gmail.com>
2023-10-31 12:08:27 +0700

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -9,52 +9,37 @@ const navigation = [
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-indigo-500 via-zinc-700/10 to-purple-600">
+
 			<nav className="my-16 animate-fade-in">
-				<ul className="flex flex-wrap items-center justify-center gap-4">
+				<ul className="flex flex-wrap items-center justify-center gap-12">
 					{navigation.map((item) => (
 						<Link
 							key={item.href}
 							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+							className="text-lg duration-500 text-zinc-200 hover:text-zinc-500"
 						>
 							{item.name}
 						</Link>
 					))}
 				</ul>
 			</nav>
+
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<Particles
 				className="absolute inset-0 -z-10 animate-fade-in"
-				quantity={100}
+				quantity={800}
 			/>
+
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				chronark
+				taufikemal
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+			
 			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-sm text-zinc-500 mx-6">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
-					<Link
-						target="_blank"
-						href="https://upstash.com"
-						className="underline duration-500 hover:text-zinc-300"
-					>
-						Upstash
-					</Link>
-
-					
-					<wbr /> and working on{" "}
-					<Link
-						target="_blank"
-						href="https://unkey.dev"
-						className="underline duration-500 hover:text-zinc-300"
-					>
-						unkey.dev
-					</Link>{" "}
-					at night.
+				<h2 className="text-lg text-zinc-400 mx-6">
+				A passionate UI/UX Designer and Brand Designer with professional experience in the education community, payment, and transportation industries.
 				</h2>
 			</div>
 		</div>
